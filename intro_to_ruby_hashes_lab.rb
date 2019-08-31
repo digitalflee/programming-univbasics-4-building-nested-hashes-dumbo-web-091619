91bd699d076fb649fa39d59f9b44798e68df6098
Keenan Alves <keenan.alves@gmail.com>
2019-08-31 21:26:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -40,7 +40,7 @@ b_hash = {:railroads => {
      :four_pieces_owned => 200
    },
    :names => {
-     :reading_railroad => { :"mortgage_value" => 100 }, 
+     :reading_railroad => { :"mortgage_value" => '$' + 100 }, 
      :pennsylvania_railroad => {},
      :b_and_o_railroad => {}, 
      :shortline_railroad => {}
