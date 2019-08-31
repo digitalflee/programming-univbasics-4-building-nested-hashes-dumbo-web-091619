8f81b94b6767a609e2416f419face19baa74b749
Keenan Alves <keenan.alves@gmail.com>
2019-08-31 21:24:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,7 +32,20 @@ end
 
 def monopoly_with_fourth_tier
 b_hash = {:railroads => {
-   :rent_in_dollars => {:names => 1} 
-	}
+   :pieces => 4,
+   :rent_in_dollars => {
+     :one_piece_owned => 25, 
+     :two_pieces_owned => 50, 
+     :three_pieces_owned => 100, 
+     :four_pieces_owned => 200
+   },
+   :names => {
+     :reading_railroad => {"mortgage_value" => $100}, 
+     :pennsylvania_railroad => {},
+     :b_and_o_railroad => {}, 
+     :shortline_railroad => {}
+   },
+   :one_piece_owned => {:fee => 25}
+ } 
 }
 end
